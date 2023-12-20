eb131cd52d060ffb5a7a8ec00dd1dc6df7ad75dd
MuratcnMtlu <101725180+MuratcnMtlu@users.noreply.github.com>
2023-12-21 02:25:05 +0300

    v1
    rs232 ve hcsr04 kodu yapıldı.

---
+++ pic_pid_controller/main.h
@@ -0,0 +1,26 @@
+#include <16F887.h>
+#device ADC=10
+#use delay(crystal=16000000)
+#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=PORT1)
+
+/*
+#define LCD_ENABLE_PIN PIN_C1
+#define LCD_RS_PIN PIN_C0
+#define LCD_RW_PIN PIN_C2
+#define LCD_DATA0 PIN_D0
+#define LCD_DATA1 PIN_D1
+#define LCD_DATA2 PIN_D2
+#define LCD_DATA3 PIN_D3
+#define LCD_DATA_PORT getenv("SFR:PORTD")
+#include <LCD.c>
+*/
+
+#define LED PIN_E1
+#define BUZZER PIN_B1
+#define trig1 PIN_B4
+#define echo1 PIN_B5
+
+#define DELAY 500
+
+float mesafe, zaman;
+
