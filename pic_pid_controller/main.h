ecc070e6148e528e685b0a3d154784d752acf5a4
MuratcnMtlu <101725180+MuratcnMtlu@users.noreply.github.com>
2023-12-22 03:14:54 +0300

    lcd deneme - fail

--- pic_pid_controller/main.h
+++ pic_pid_controller/main.h
@@ -3,17 +3,16 @@
 #use delay(crystal=16000000)
 #use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=PORT1)
 
-/*
-#define LCD_ENABLE_PIN PIN_C1
-#define LCD_RS_PIN PIN_C0
-#define LCD_RW_PIN PIN_C2
-#define LCD_DATA0 PIN_D0
-#define LCD_DATA1 PIN_D1
-#define LCD_DATA2 PIN_D2
-#define LCD_DATA3 PIN_D3
-#define LCD_DATA_PORT getenv("SFR:PORTD")
+
+#define LCD_ENABLE_PIN  PIN_C2                                   
+#define LCD_RS_PIN      PIN_C3                                  
+#define LCD_RW_PIN      PIN_C1                                   
+#define LCD_DATA4       PIN_D0                                
+#define LCD_DATA5       PIN_D1                                  
+#define LCD_DATA6       PIN_D2                                   
+#define LCD_DATA7       PIN_D3
 #include <LCD.c>
-*/
+
 
 #define LED PIN_E1
 #define BUZZER PIN_B1
